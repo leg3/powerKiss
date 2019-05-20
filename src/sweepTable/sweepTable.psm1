6852e77cf8562383f7927aeacc8762254e7e4484
Temistocles R. Valdes <valdestr@leg3.net>
2019-05-20 16:12:46 -0400

    functionality change

--- src/sweepTable/sweepTable.psm1
+++ src/sweepTable/sweepTable.psm1
@@ -1,9 +1,11 @@
 function sweepTable {
-Get-ChildItem -Path ${psscriptroot} -Name -Filter "*.netxml" | ForEach-Object {
-$name = $_.Replace(".netxml","")
-mkdir $name
-$destination = Get-ChildItem -Path ./ -Directory $name
-Move-Item -Path ".\$name*.csv" -Destination $destination
-Move-Item -Path ".\$name*.netxml" -Destination $destination    
-}
+
+	Get-ChildItem -Name -Filter "*.netxml" | ForEach-Object {
+		$name = $_.Replace(".netxml","")
+		mkdir $name
+		$destination = Get-ChildItem -Path ./ -Directory $name
+		Move-Item -Path ".\$name*.csv" -Destination $destination
+		Move-Item -Path ".\$name*.netxml" -Destination $destination
+    }
+    
 }
