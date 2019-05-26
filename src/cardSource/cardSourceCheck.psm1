9ef914c3d377af62bf88685262f44d1b081b55b3
Temistocles R. Valdes <valdestr@voodoo0x07.leg3.net>
2019-05-26 14:55:11 -0400

    cardSourceCheck simple implementation

--- src/cardSource/cardSourceCheck.psm1
+++ src/cardSource/cardSourceCheck.psm1
@@ -1,7 +1,19 @@
-# Log the count from our .csv files for reference
-Get-ChildItem -Recurse -Name -Filter "*_cardSource.csv" | ForEach-Object {
+function cardSourceCheck {
 
-    $_ | Out-File log.txt -Append
-    Import-Csv $_ | Measure-Object | Out-File log.txt -Append
-  
-  }
+	if (-not (Test-Path "./*cardSource.csv")) { Write-Host "cardSource Table - ABSENT!" }
+	if (-not (Test-Path "./*detectionRun.csv")) { Write-Host "detectionRun Table - ABSENT!" }
+	if (-not (Test-Path "./*wirelessClient.csv")) { Write-Host "wirelessClient Table - ABSENT!" }
+	if (-not (Test-Path "./*wirelessClientGPSInfo.csv")) { Write-Host "wirelessClientGPSInfo Table - ABSENT!" }
+	if (-not (Test-Path "./*wirelessClientPackets.csv")) { Write-Host "wirelessClientPackets Table - ABSENT!" }
+	if (-not (Test-Path "./*wirelessClientSNRInfo.csv")) { Write-Host "wirelessClientSNRInfo Table - ABSENT!" }
+	if (-not (Test-Path "./*wirelessClientSeenCard.csv")) { Write-Host "wirelessClientSeenCard Table - ABSENT!" }
+	if (-not (Test-Path "./*wirelessNetwork.csv")) { Write-Host "wirelessNetwork Table - ABSENT!" }
+	if (-not (Test-Path "./*wirelessNetworkFreqMHZ.csv")) { Write-Host "wirelessNetworkFreqMHZ Table - ABSENT!" }
+	if (-not (Test-Path "./*wirelessNetworkGPSInfo.csv")) { Write-Host "wirelessNetworkGPSInfo Table - ABSENT!" }
+	if (-not (Test-Path "./*wirelessNetworkPackets.csv")) { Write-Host "wirelessNetworkPackets Table - ABSENT!" }
+	if (-not (Test-Path "./*wirelessNetworkSNRInfo.csv")) { Write-Host "wirelessNetworkSNRInfo Table - ABSENT!" }
+	if (-not (Test-Path "./*wirelessNetworkSSID.csv")) { Write-Host "wirelessNetworkSSID Table - ABSENT!" }
+	if (-not (Test-Path "./*wirelessNetworkSSIDEncryption.csv")) { Write-Host "wirelessNetworkSSIDEncryption Table - ABSENT!" }
+	if (-not (Test-Path "./*wirelessNetworkSeenCard.csv")) { Write-Host "wirelessNetworkSeenCard Table - ABSENT!" }
+
+}
