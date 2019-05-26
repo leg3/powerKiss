b140231ad455f00e210575f590bff0f8b280b51b
Temistocles R. Valdes <valdestr@voodoo0x07.leg3.net>
2019-05-26 11:51:45 -0400

    readded null test

--- src/wirelessNetworkFreqMHZ/wirelessNetworkFreqMHZ.psm1
+++ src/wirelessNetworkFreqMHZ/wirelessNetworkFreqMHZ.psm1
@@ -1,34 +1,38 @@
 function wirelessNetworkFreqMHZ ($inputPath,$outputPath) {
 
-  # load the .netxml file to be processed
-  [xml]$xml = Get-Content $inputPath
+	# load the .netxml file to be processed
+	[xml]$xml = Get-Content $inputPath
 
-  # Loop through the child nodes of the selected nodes and assemble an object to hold our values.
-  $xml.'detection-run'.'wireless-network' | ForEach-Object {
+	# Loop through the child nodes of the selected nodes and assemble an object to hold our values.
+	$xml.'detection-run'.'wireless-network' | ForEach-Object {
 
-    # Get the index of the current row and use it to retrieve the corresponding BSSID value
-    $index = $xml.'detection-run'.'wireless-network'.IndexOf($_);
-    $BSSID = $xml.'detection-run'.'wireless-network'.'BSSID'[$index]
+		# Test to see if the row has valid data and build the composite object
+		if ($_ -ne $NULL) {
 
-    # Loop through the child nodes of the selected nodes and assemble an object to hold our values.
-    $xml.'detection-run'.'wireless-network'.'freqmhz' | ForEach-Object {
+			# Get the index of the current row and use it to retrieve the corresponding BSSID value
+			$index = $xml.'detection-run'.'wireless-network'.IndexOf($_);
+			$BSSID = $xml.'detection-run'.'wireless-network'.'BSSID'[$index]
 
-      # Create the wirelessNetworkFreqMHZ object
-      $wirelessNetworkFreqMHZ = New-Object Object
+			# Loop through the child nodes of the selected nodes and assemble an object to hold our values.
+			$xml.'detection-run'.'wireless-network'.'freqmhz' | ForEach-Object {
 
-      # Get the start time of the data set
-      $startTime = $xml.'detection-run'.'start-time'
+				# Create the wirelessNetworkFreqMHZ object
+				$wirelessNetworkFreqMHZ = New-Object Object
 
-      # Add our values to the composite object
-      Add-Member -InputObject $wirelessNetworkFreqMHZ -MemberType NoteProperty -Name "freqmhz" -Value $_
-      Add-Member -InputObject $wirelessNetworkFreqMHZ -MemberType NoteProperty -Name "bssid" -Value $BSSID
-      Add-Member -InputObject $wirelessNetworkFreqMHZ -MemberType NoteProperty -Name "start-time" -Value $startTime
+				# Get the start time of the data set
+				$startTime = $xml.'detection-run'.'start-time'
 
-      # Export a .csv file of the composite object values
-      $label = ($inputPath.Replace(".netxml","") + "_wirelessNetworkFreqMHZ.csv")
-      $wirelessNetworkFreqMHZ | Export-Csv -Path "$outputPath\$label" -Append -NoTypeInformation
+				# Add our values to the composite object
+				Add-Member -InputObject $wirelessNetworkFreqMHZ -MemberType NoteProperty -Name "freqmhz" -Value $_
+				Add-Member -InputObject $wirelessNetworkFreqMHZ -MemberType NoteProperty -Name "bssid" -Value $BSSID
+				Add-Member -InputObject $wirelessNetworkFreqMHZ -MemberType NoteProperty -Name "start-time" -Value $startTime
 
-    }
-  }
+				# Export a .csv file of the composite object values
+				$label = ($inputPath.Replace(".netxml","") + "_wirelessNetworkFreqMHZ.csv")
+				$wirelessNetworkFreqMHZ | Export-Csv -Path "$outputPath\$label" -Append -NoTypeInformation
+
+			}
+		}
+	}
 }
 
--- src/wirelessNetworkSSIDEncryption/wirelessNetworkSSIDEncryption.psm1
+++ src/wirelessNetworkSSIDEncryption/wirelessNetworkSSIDEncryption.psm1
@@ -1,29 +1,33 @@
 function wirelessNetworkSSIDEncryption ($inputPath,$outputPath) {
 
-  # load the .netxml file to be processed
-  [xml]$xml = Get-Content $inputPath
+	# load the .netxml file to be processed
+	[xml]$xml = Get-Content $inputPath
 
-  # Get the index of the current row and use it to retrieve the corresponding BSSID value
-  $xml.'detection-run'.'wireless-network'.'ssid' | ForEach-Object {
+	# Test to see if the row has valid data and build the composite object
+	if ($_ -ne $NULL) {
 
-    $index = $xml.'detection-run'.'wireless-network'.'ssid'.IndexOf($_);
-    $BSSID = $xml.'detection-run'.'wireless-network'.'BSSID'[$index]
+		# Get the index of the current row and use it to retrieve the corresponding BSSID value
+		$xml.'detection-run'.'wireless-network'.'ssid' | ForEach-Object {
 
-    # Create the wirelessNetworkSSIDEncryption object
-    $xml.'detection-run'.'wireless-network'.'ssid'.'encryption' | ForEach-Object {
+			$index = $xml.'detection-run'.'wireless-network'.'ssid'.IndexOf($_);
+			$BSSID = $xml.'detection-run'.'wireless-network'.'BSSID'[$index]
 
-      $wirelessNetworkSSIDEncryption = New-Object Object
-      $startTime = $xml.'detection-run'.'start-time' # Get the start time value from the root node
+			# Create the wirelessNetworkSSIDEncryption object
+			$xml.'detection-run'.'wireless-network'.'ssid'.'encryption' | ForEach-Object {
 
-      # Define the columns for the object and add the current values for this iteration
-      Add-Member -InputObject $wirelessNetworkSSIDEncryption -MemberType NoteProperty -Name "encryption" -Value $_
-      Add-Member -InputObject $wirelessNetworkSSIDEncryption -MemberType NoteProperty -Name "BSSID" -Value $BSSID
-      Add-Member -InputObject $wirelessNetworkSSIDEncryption -MemberType NoteProperty -Name "start-time" -Value $startTime
+				$wirelessNetworkSSIDEncryption = New-Object Object
+				$startTime = $xml.'detection-run'.'start-time' # Get the start time value from the root node
 
-      # Create and append the resultant .csv file that will be the wirelessNetworkSSIDEncryption table
-      $label = ($inputPath.Replace(".netxml","") + "_wirelessNetworkSSIDEncryption.csv")
-      $wirelessNetworkSSIDEncryption | Export-Csv -Path "$outputPath\$label" -Append -NoTypeInformation
+				# Define the columns for the object and add the current values for this iteration
+				Add-Member -InputObject $wirelessNetworkSSIDEncryption -MemberType NoteProperty -Name "encryption" -Value $_
+				Add-Member -InputObject $wirelessNetworkSSIDEncryption -MemberType NoteProperty -Name "BSSID" -Value $BSSID
+				Add-Member -InputObject $wirelessNetworkSSIDEncryption -MemberType NoteProperty -Name "start-time" -Value $startTime
 
-    }
-  }
+				# Create and append the resultant .csv file that will be the wirelessNetworkSSIDEncryption table
+				$label = ($inputPath.Replace(".netxml","") + "_wirelessNetworkSSIDEncryption.csv")
+				$wirelessNetworkSSIDEncryption | Export-Csv -Path "$outputPath\$label" -Append -NoTypeInformation
+
+			}
+		}
+	}
 }
